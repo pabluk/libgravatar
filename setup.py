c34f56429be97c317506395f55b1bafcb380fc07
Pablo SEMINARIO <pabluk@gmail.com>
2013-03-10 01:27:34 +0100

    Updated version number

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.rst') as file:
 
 
 setup(name='libgravatar',
-      version='0.1',
+      version='0.2.0',
       author='Pablo SEMINARIO',
       author_email='pabluk@gmail.com',
       url='https://github.com/pabluk/libgravatar',
@@ -14,7 +14,7 @@ setup(name='libgravatar',
       description='A library that provides a Python 3 interface for the Gravatar API.',
       long_description=long_description,
       packages=['libgravatar'],
-      provides=['libgravatar (0.1)'],
+      provides=['libgravatar (0.2.0)'],
       classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
