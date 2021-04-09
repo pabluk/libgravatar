8b694b86956c0cb7c73aa4b489a7a8b73212ab10
Pablo Seminario <pablo@seminar.io>
2021-04-10 00:52:03 +0200

    Re-formatted Python files using black

--- tests/test_gravatar.py
+++ tests/test_gravatar.py
@@ -7,17 +7,19 @@ class GravatarImageTestCase(unittest.TestCase):
     """Test case for the Gravatar class and the get_image method."""
 
     def setUp(self):
-        self.g = Gravatar('myemailaddress@example.com')
+        self.g = Gravatar("myemailaddress@example.com")
 
     def test_without_params(self):
         """Test a get_image with default parameters."""
-        image_url = 'https://www.gravatar.com/avatar/0bc83cb571cd1c50ba6f3e8a78ef1346'
+        image_url = "https://www.gravatar.com/avatar/0bc83cb571cd1c50ba6f3e8a78ef1346"
         result = self.g.get_image()
         self.assertEqual(image_url, result)
 
     def test_image_size(self):
         """Test image size parameter."""
-        image_url = 'https://www.gravatar.com/avatar/0bc83cb571cd1c50ba6f3e8a78ef1346?size=24'
+        image_url = (
+            "https://www.gravatar.com/avatar/0bc83cb571cd1c50ba6f3e8a78ef1346?size=24"
+        )
         result = self.g.get_image(size=24)
         self.assertEqual(image_url, result)
 
@@ -28,73 +30,80 @@ class GravatarImageTestCase(unittest.TestCase):
 
     def test_filetype_extension(self):
         """Test filetype_extension parameter."""
-        image_url = 'https://www.gravatar.com/avatar/0bc83cb571cd1c50ba6f3e8a78ef1346.jpg'
+        image_url = (
+            "https://www.gravatar.com/avatar/0bc83cb571cd1c50ba6f3e8a78ef1346.jpg"
+        )
         result = self.g.get_image(filetype_extension=True)
         self.assertEqual(image_url, result)
 
     def test_default(self):
         """Test default parameter."""
-        image_url = 'https://www.gravatar.com/avatar/0bc83cb571cd1c50ba6f3e8a78ef1346?default=monsterid'
-        result = self.g.get_image(default='monsterid')
+        image_url = "https://www.gravatar.com/avatar/0bc83cb571cd1c50ba6f3e8a78ef1346?default=monsterid"
+        result = self.g.get_image(default="monsterid")
         self.assertEqual(image_url, result)
 
-        image_url = 'https://www.gravatar.com/avatar/0bc83cb571cd1c50ba6f3e8a78ef1346?default=http%3A%2F%2Fexample.com%2Fimages%2Favatar.jpg'
-        result = self.g.get_image(default='http://example.com/images/avatar.jpg')
+        image_url = "https://www.gravatar.com/avatar/0bc83cb571cd1c50ba6f3e8a78ef1346?default=http%3A%2F%2Fexample.com%2Fimages%2Favatar.jpg"
+        result = self.g.get_image(default="http://example.com/images/avatar.jpg")
         self.assertEqual(image_url, result)
 
-        invalid_default_url = 'ftp://example.com/images/avatar.php?key=value'
+        invalid_default_url = "ftp://example.com/images/avatar.php?key=value"
         self.assertRaises(ValueError, self.g.get_image, default=invalid_default_url)
 
     def test_forcedefault(self):
         """Test forcedefault parameter."""
-        image_url = 'https://www.gravatar.com/avatar/0bc83cb571cd1c50ba6f3e8a78ef1346?forcedefault=y'
+        image_url = "https://www.gravatar.com/avatar/0bc83cb571cd1c50ba6f3e8a78ef1346?forcedefault=y"
         result = self.g.get_image(force_default=True)
         self.assertEqual(image_url, result)
 
     def test_use_ssl(self):
         """Test use_ssl parameter."""
-        image_url = 'http://www.gravatar.com/avatar/0bc83cb571cd1c50ba6f3e8a78ef1346'
+        image_url = "http://www.gravatar.com/avatar/0bc83cb571cd1c50ba6f3e8a78ef1346"
         result = self.g.get_image(use_ssl=False)
         self.assertEqual(image_url, result)
 
     def test_rating(self):
         """Test the rating parameter."""
-        image_url = 'https://www.gravatar.com/avatar/0bc83cb571cd1c50ba6f3e8a78ef1346?rating=g'
-        result = self.g.get_image(rating='g')
+        image_url = (
+            "https://www.gravatar.com/avatar/0bc83cb571cd1c50ba6f3e8a78ef1346?rating=g"
+        )
+        result = self.g.get_image(rating="g")
         self.assertEqual(image_url, result)
 
-        image_url = 'https://www.gravatar.com/avatar/0bc83cb571cd1c50ba6f3e8a78ef1346?rating=pg'
-        result = self.g.get_image(rating='pg')
+        image_url = (
+            "https://www.gravatar.com/avatar/0bc83cb571cd1c50ba6f3e8a78ef1346?rating=pg"
+        )
+        result = self.g.get_image(rating="pg")
         self.assertEqual(image_url, result)
 
-        image_url = 'https://www.gravatar.com/avatar/0bc83cb571cd1c50ba6f3e8a78ef1346?rating=r'
-        result = self.g.get_image(rating='r')
+        image_url = (
+            "https://www.gravatar.com/avatar/0bc83cb571cd1c50ba6f3e8a78ef1346?rating=r"
+        )
+        result = self.g.get_image(rating="r")
         self.assertEqual(image_url, result)
 
-        image_url = 'https://www.gravatar.com/avatar/0bc83cb571cd1c50ba6f3e8a78ef1346?rating=x'
-        result = self.g.get_image(rating='x')
+        image_url = (
+            "https://www.gravatar.com/avatar/0bc83cb571cd1c50ba6f3e8a78ef1346?rating=x"
+        )
+        result = self.g.get_image(rating="x")
         self.assertEqual(image_url, result)
 
-        self.assertRaises(ValueError, self.g.get_image, rating='invalid')
+        self.assertRaises(ValueError, self.g.get_image, rating="invalid")
 
 
 class GravatarProfileTestCase(unittest.TestCase):
     """Test case for the Gravatar class and the get_profile method."""
 
     def setUp(self):
-        self.g = Gravatar('myemailaddress@example.com')
+        self.g = Gravatar("myemailaddress@example.com")
 
     def test_without_params(self):
         """Test a get_profile with default parameters."""
-        profile_url = 'https://www.gravatar.com/0bc83cb571cd1c50ba6f3e8a78ef1346'
+        profile_url = "https://www.gravatar.com/0bc83cb571cd1c50ba6f3e8a78ef1346"
         result = self.g.get_profile()
         self.assertEqual(profile_url, result)
 
     def test_use_ssl(self):
         """Test use_ssl parameter."""
-        image_url = 'http://www.gravatar.com/0bc83cb571cd1c50ba6f3e8a78ef1346'
+        image_url = "http://www.gravatar.com/0bc83cb571cd1c50ba6f3e8a78ef1346"
         result = self.g.get_profile(use_ssl=False)
         self.assertEqual(image_url, result)
-
-
-
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -8,13 +8,13 @@ class SanitizeEmailTestCase(unittest.TestCase):
 
     def test_valid_email(self):
         """Test a valid email address."""
-        email = sanitize_email('myemailaddress@example.com')
-        self.assertEqual(email, 'myemailaddress@example.com')
+        email = sanitize_email("myemailaddress@example.com")
+        self.assertEqual(email, "myemailaddress@example.com")
 
     def test_denormalized_email(self):
         """Test a denormalized email address."""
-        email = sanitize_email(' MyEmailAddress@example.com ')
-        self.assertEqual(email, 'myemailaddress@example.com')
+        email = sanitize_email(" MyEmailAddress@example.com ")
+        self.assertEqual(email, "myemailaddress@example.com")
 
 
 class MD5HashTestCase(unittest.TestCase):
@@ -22,13 +22,13 @@ class MD5HashTestCase(unittest.TestCase):
 
     def test_md5_hash_from_email(self):
         """Test a md5 hash from an email address."""
-        generated_hash = md5_hash('myemailaddress@example.com')
-        self.assertEqual(generated_hash, '0bc83cb571cd1c50ba6f3e8a78ef1346')
+        generated_hash = md5_hash("myemailaddress@example.com")
+        self.assertEqual(generated_hash, "0bc83cb571cd1c50ba6f3e8a78ef1346")
 
     def test_md5_hash_with_empty_string(self):
         """Test a md5 hash for an empty string."""
-        generated_hash = md5_hash('')
-        self.assertEqual(generated_hash, 'd41d8cd98f00b204e9800998ecf8427e')
+        generated_hash = md5_hash("")
+        self.assertEqual(generated_hash, "d41d8cd98f00b204e9800998ecf8427e")
 
 
 class DefaultURLTestCase(unittest.TestCase):
@@ -36,32 +36,32 @@ class DefaultURLTestCase(unittest.TestCase):
 
     def test_a_valid_url(self):
         """Test a valid URL."""
-        url = 'http://example.com/images/avatar.jpg'
+        url = "http://example.com/images/avatar.jpg"
         self.assertTrue(default_url_is_valid(url))
 
-        url = 'https://example.com/images/avatar.jpg'
+        url = "https://example.com/images/avatar.jpg"
         self.assertTrue(default_url_is_valid(url))
 
-        url = 'http://example.com/images/avatar.png'
+        url = "http://example.com/images/avatar.png"
         self.assertTrue(default_url_is_valid(url))
 
-        url = 'http://example.com/images/avatar.jpeg'
+        url = "http://example.com/images/avatar.jpeg"
         self.assertTrue(default_url_is_valid(url))
 
-        url = 'http://example.com/images/avatar.gif'
+        url = "http://example.com/images/avatar.gif"
         self.assertTrue(default_url_is_valid(url))
 
     def test_invalid_protocol(self):
         """Test an URL with an invalid protocol."""
-        url = 'ftp://example.com/images/avatar.jpg'
+        url = "ftp://example.com/images/avatar.jpg"
         self.assertFalse(default_url_is_valid(url))
 
     def test_invalid_extension(self):
         """Test an URL with an invalid extension file."""
-        url = 'http://example.com/images/avatar.php'
+        url = "http://example.com/images/avatar.php"
         self.assertFalse(default_url_is_valid(url))
 
     def test_invalid_with_query(self):
         """Test an URL with an invalid query."""
-        url = 'http://example.com/images/avatar.jpg?key=value'
+        url = "http://example.com/images/avatar.jpg?key=value"
         self.assertFalse(default_url_is_valid(url))
